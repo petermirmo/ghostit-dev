49e83a00646aaf05c5edfd0e29b65237203e6096
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2018-02-09 00:39:24 -0800

    Reformatted index and routes files

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./routes";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import reduxThunk from "redux-thunk";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from './routes'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk'
 
-import reducers from "./reducers";
+import reducers from './reducers'
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Routes />
-    </Provider>,
-    document.getElementById("root")
-);
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('root'),
+)
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import * as actions from "./actions";
-import LoginPage from "./pages/LoginPage";
-import Content from "./pages/ContentPage";
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from './actions'
+import LoginPage from './pages/LoginPage'
+import Content from './pages/ContentPage'
 
 class Routes extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Route path="/" exact={true} component={LoginPage} />
-                    <Route path="/content" exact={true} component={Content} />
-                </div>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    // TODO: Add a check that the user is logged in, if not, then redirect to path="/login"
+    return (
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact={true} component={LoginPage} />
+          <Route path="/content" exact={true} component={Content} />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default connect(null, actions)(Routes);
+export default connect(null, actions)(Routes)
