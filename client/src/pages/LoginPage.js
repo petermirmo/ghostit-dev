e4277ea7e1a79ac4a1eced4a08d0056365329958
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2018-02-09 00:39:42 -0800

    Moved form toggle to react state

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,134 +1,90 @@
-import React, { Component } from "react";
-import "../css/theme.css";
+import React, { Component } from 'react'
+import '../css/theme.css'
 
-function changeToRegistrationForm() {
-    document.getElementById("loginForm").style.display = "none";
-    document.getElementById("registerForm").style.display = "";
-}
-function changeToLoginForm() {
-    document.getElementById("registerForm").style.display = "none";
-    document.getElementById("loginForm").style.display = "";
+const styles = {
+  cursorPointer: {
+    cursor: 'pointer',
+  },
+  hr: {
+    border: '2px solid #eeeeef',
+    width: '90%',
+  },
 }
 
 class Login extends Component {
-    render() {
-        return (
-            <div className="login-background">
-                <div className="login-box">
-                    <div id="loginForm">
-                        <form
-                            className="form-box"
-                            action="/api/auth/login"
-                            method="post"
-                        >
-                            <input
-                                id="emailLoginInput"
-                                type="text"
-                                name="email"
-                                placeholder="Email"
-                            />
-                            <hr
-                                style={{
-                                    border: "2px solid #eeeeef",
-                                    width: "90%"
-                                }}
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                            />
-                            <input
-                                className="submit-colorful"
-                                value="Sign In"
-                                type="submit"
-                            />
-                        </form>
+  state = {
+    loginForm: true,
+  }
+  render() {
+    const { loginForm } = this.state
+    let form
+    if (loginForm) {
+      form = (
+        <div id="loginForm">
+          <form className="form-box" action="/api/auth/login" method="post">
+            <input
+              id="emailLoginInput"
+              type="text"
+              name="email"
+              placeholder="Email"
+            />
+            <hr style={styles.hr} />
+            <input type="password" name="password" placeholder="Password" />
+            <input className="submit-colorful" value="Sign In" type="submit" />
+          </form>
 
-                        <br />
-                        <p
-                            id="registerButton"
-                            href="#"
-                            onClick={changeToRegistrationForm}
-                            style={{ cursor: "pointer" }}
-                        >
-                            Dont have an account? Get started here!
-                        </p>
-                    </div>
+          <br />
+          <p
+            onClick={() => this.setState({ loginForm: false })}
+            style={styles.cursorPointer}
+          >
+            Dont have an account? Get started here!
+          </p>
+        </div>
+      )
+    } else {
+      form = (
+        <div id="registerForm">
+          <form
+            className="form-box"
+            action="api/auth/email/register"
+            method="post"
+          >
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Full Name"
+              required
+            />
+            <hr style={styles.hr} />
+            <input type="text" name="email" placeholder="Email" />
+            <hr style={styles.hr} />
+            <input type="text" name="website" placeholder="Website" />
+            <hr style={styles.hr} />
+            <input type="password" name="password" placeholder="Password" />
+            <hr style={styles.hr} />
 
-                    <div id="registerForm" style={{ display: "none" }}>
-                        <form
-                            className="form-box"
-                            action="api/auth/email/register"
-                            method="post"
-                        >
-                            <input
-                                type="text"
-                                name="fullName"
-                                placeholder="Full Name"
-                                required
-                            />
-                            <hr
-                                style={{
-                                    border: "2px solid #eeeeef",
-                                    width: "90%"
-                                }}
-                            />
-                            <input
-                                type="text"
-                                name="email"
-                                placeholder="Email"
-                            />
-                            <hr
-                                style={{
-                                    border: "2px solid #eeeeef",
-                                    width: "90%"
-                                }}
-                            />
-                            <input
-                                type="text"
-                                name="website"
-                                placeholder="Website"
-                            />
-                            <hr
-                                style={{
-                                    border: "2px solid #eeeeef",
-                                    width: "90%"
-                                }}
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                            />
-                            <hr
-                                style={{
-                                    border: "2px solid #eeeeef",
-                                    width: "90%"
-                                }}
-                            />
+            <input className="submit-colorful" type="submit" value="Register" />
+          </form>
 
-                            <input
-                                className="submit-colorful"
-                                type="submit"
-                                value="Register"
-                            />
-                        </form>
-
-                        <br />
-                        <p
-                            id="registerButton"
-                            href="#"
-                            onClick={changeToLoginForm}
-                            style={{ cursor: "pointer" }}
-                        >
-                            Have an account? Sign in!
-                        </p>
-                    </div>
-                </div>
-            </div>
-        );
+          <br />
+          <p
+            onClick={() => this.setState({ loginForm: true })}
+            style={styles.cursorPointer}
+          >
+            Have an account? Sign in!
+          </p>
+        </div>
+      )
     }
+    return (
+      <div className="login-background">
+        <div className="login-box">
+          {form}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Login;
+export default Login
