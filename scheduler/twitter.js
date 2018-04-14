90ae637492719aa445360e992bd20c1867dac1ca
petermirmo <peter.mirmotahari@gmail.com>
2018-04-14 13:02:30 -0500

    Twitter post failures bug fix

--- scheduler/twitter.js
+++ scheduler/twitter.js
@@ -6,89 +6,77 @@ var request = require("request").defaults({ encoding: null });
 var Twitter = require("twitter");
 
 module.exports = {
-    postToProfile: function(post) {
-        Account.findOne(
-            {
-                _id: post.accountID
-            },
-            async function(err, account) {
-                // Initialize client
-                var client = new Twitter({
-                    consumer_key: keys.twitterConsumerKey,
-                    consumer_secret: keys.twitterConsumerSecret,
-                    access_token_key: account.accessToken,
-                    access_token_secret: account.tokenSecret
-                });
+	postToProfile: function(post) {
+		Account.findOne(
+			{
+				_id: post.accountID
+			},
+			async function(err, account) {
+				// Initialize client
+				var client = new Twitter({
+					consumer_key: keys.twitterConsumerKey,
+					consumer_secret: keys.twitterConsumerSecret,
+					access_token_key: account.accessToken,
+					access_token_secret: account.tokenSecret
+				});
 
-                // Check if this post has images in it
-                if (post.images.length !== 0) {
-                    uploadImage("", 0, client, post);
-                } else {
-                    // If no pictures then just post normally
-                    postToTwitter(client, { status: post.content }, post._id);
-                }
-            }
-        );
-    }
+				// Check if this post has images in it
+				if (post.images.length !== 0) {
+					uploadImage("", 0, client, post);
+				} else {
+					// If no pictures then just post normally
+					postToTwitter(client, { status: post.content }, post._id);
+				}
+			}
+		);
+	}
 };
 function uploadImage(mediaListID, i, client, post) {
-    // If this post has images we have a recursive function to add each photo
-    // and store the media_id in the mediaListID string
-    if (post.images[i]) {
-        // Download image from url
-        request.get(post.images[i].imageURL, function(err, res, body) {
-            // Upload image to Twitter
-            client.post("media/upload", { media: body }, function(
-                error,
-                media,
-                response
-            ) {
-                if (error || media.media_id === undefined) {
-                    savePostError(postID, error);
-                } else {
-                    mediaListID += "," + media.media_id_string;
-                    uploadImage(mediaListID, i + 1, client, post);
-                }
-            });
-        });
-    } else {
-        // If there are no more images to upload then post to Twitter
-        mediaListID = mediaListID.slice(1, mediaListID.length);
-        postToTwitter(
-            client,
-            { status: post.content, media_ids: mediaListID },
-            post._id
-        );
-    }
+	// If this post has images we have a recursive function to add each photo
+	// and store the media_id in the mediaListID string
+	if (post.images[i]) {
+		// Download image from url
+		request.get(post.images[i].imageURL, function(err, res, body) {
+			// Upload image to Twitter
+			client.post("media/upload", { media: body }, function(error, media, response) {
+				if (error || media.media_id === undefined) {
+					savePostError(post._id, error);
+				} else {
+					mediaListID += "," + media.media_id_string;
+					uploadImage(mediaListID, i + 1, client, post);
+				}
+			});
+		});
+	} else {
+		// If there are no more images to upload then post to Twitter
+		mediaListID = mediaListID.slice(1, mediaListID.length);
+		postToTwitter(client, { status: post.content, media_ids: mediaListID }, post._id);
+	}
 }
 function postToTwitter(client, twitterPost, postID) {
-    client.post("statuses/update", twitterPost, function(
-        error,
-        tweet,
-        response
-    ) {
-        if (error || tweet.id === undefined) {
-            savePostError(postID, error);
-        } else {
-            savePostSuccessfully(postID, tweet.id);
-        }
-    });
+	client.post("statuses/update", twitterPost, function(error, tweet, response) {
+		if (error || tweet.id === undefined) {
+			savePostError(postID, error);
+		} else {
+			savePostSuccessfully(postID, tweet.id);
+		}
+	});
 }
 function savePostError(postID, error) {
-    Post.findOne({ _id: postID }, function(err, post) {
-        post.status = "error";
-        post.errorMessage = JSON.stringify(error);
-        post.save().then(response => {
-            return;
-        });
-    });
+	Post.findOne({ _id: postID }, function(err, post) {
+		post.status = "error";
+		post.errorMessage = JSON.stringify(error);
+		post.save().then(response => {
+			return;
+		});
+	});
 }
 function savePostSuccessfully(postID, twitterPostID) {
-    Post.findOne({ _id: postID }, function(err, result) {
-        result.status = "posted";
-        result.socialMediaID = twitterPostID;
-        result.save().then(response => {
-            return;
-        });
-    });
+	Post.findOne({ _id: postID }, function(err, result) {
+		result.status = "posted";
+		result.socialMediaID = twitterPostID;
+		result.save().then(response => {
+			return;
+		});
+	});
 }
