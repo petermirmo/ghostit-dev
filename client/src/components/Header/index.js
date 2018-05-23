73eff54eef778cda8f9441f1baa4c0b72c1cbad7
petermirmo <peter.mirmotahari@gmail.com>
2018-05-23 15:03:26 -0700

    Logos in header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import "font-awesome/css/font-awesome.min.css";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -53,48 +52,47 @@ class Header extends Component {
 		return (
 			<header>
 				<div className="navbar">
-					<div className="dropdown">
-						{<button className={"dropbtn" + this.isActive("profile")}>Profile</button>}
-
-						<div className="dropdown-content">
-							<a onClick={() => changePage("profile")}>Profile</a>
-							<a onClick={() => this.logout()}>Logout</a>
-						</div>
-					</div>
-
-					{isAdmin && (
-						<a className={"header-button" + this.isActive("manage")} onClick={() => changePage("manage")}>
-							Manage
-						</a>
-					)}
-					{
-						<a className={"header-button" + this.isActive("accounts")} onClick={() => changePage("accounts")}>
-							Social Profiles
-						</a>
-					}
-					{
-						<a className={"header-button" + this.isActive("strategy")} onClick={() => changePage("strategy")}>
-							Strategy
-						</a>
-					}
-
-					{
-						<a className={"header-button" + this.isActive("content")} onClick={() => changePage("content")}>
-							Content
-						</a>
-					}
-
-					{(user.role === "demo" || isAdmin) && (
-						<a className={"header-button" + this.isActive("subscribe")} onClick={() => changePage("subscribe")}>
-							Become Awesome
-						</a>
-					)}
-
 					{(user.role === "manager" || isAdmin) && (
 						<button className="big-round-button" onClick={() => this.openClientSideBar()}>
 							My Clients
 						</button>
 					)}
+					<div className="main-nav">
+						{(user.role === "demo" || isAdmin) && (
+							<a className={"header-button" + this.isActive("subscribe")} onClick={() => changePage("subscribe")}>
+								Become Awesome
+							</a>
+						)}
+						{
+							<a className={"header-button" + this.isActive("content")} onClick={() => changePage("content")}>
+								Calendar
+							</a>
+						}
+						{
+							<a className={"header-button" + this.isActive("strategy")} onClick={() => changePage("strategy")}>
+								Strategy
+							</a>
+						}
+						{
+							<a className={"header-button" + this.isActive("accounts")} onClick={() => changePage("accounts")}>
+								Social Profiles
+							</a>
+						}
+						{isAdmin && (
+							<button
+								className={"header-icon fa fa-cogs" + this.isActive("manage")}
+								onClick={() => changePage("manage")}
+							/>
+						)}
+						<div className="dropdown">
+							<button className={"header-icon fa fa-user" + this.isActive("profile")} />
+
+							<div className="dropdown-content">
+								<a onClick={() => changePage("profile")}>Profile</a>
+								<a onClick={() => this.logout()}>Logout</a>
+							</div>
+						</div>
+					</div>
 				</div>
 
 				{(activePage === "content" || activePage === "strategy" || activePage === "subscribe") &&
