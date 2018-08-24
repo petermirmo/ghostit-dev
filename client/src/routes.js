a40982b887121acf1a59d30bb01c7297a7b60c3e
petermirmo <peter.mirmotahari@gmail.com>
2018-08-24 16:26:49 -0700

    Quick fix

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,7 +6,13 @@ import faTimes from "@fortawesome/fontawesome-free-solid/faTimes";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { changePage, setUser, updateAccounts, openHeaderSideBar, openClientSideBar } from "./redux/actions/";
+import {
+  changePage,
+  setUser,
+  updateAccounts,
+  openHeaderSideBar,
+  openClientSideBar
+} from "./redux/actions/";
 
 import LoaderWedge from "./components/Notifications/LoaderWedge";
 
@@ -27,129 +33,139 @@ import WritersBrief from "./pages/WritersBriefPage/";
 import "./css/";
 
 class Routes extends Component {
-	state = {
-		datebaseConnection: false
-	};
-	constructor(props) {
-		super(props);
+  state = {
+    datebaseConnection: false
+  };
+  constructor(props) {
+    super(props);
 
-		axios.get("/api/user").then(res => {
-			let { success, user } = res.data;
+    axios.get("/api/user").then(res => {
+      let { success, user } = res.data;
 
-			if (success) {
-				// Get all connected accounts of the user
-				axios.get("/api/accounts").then(res => {
-					// Set user's accounts to state
-					let { accounts } = res.data;
-					if (!accounts) accounts = [];
-					props.updateAccounts(accounts);
-					props.setUser(user);
-					props.changePage("subscribe");
-					this.setState({ datebaseConnection: true });
-				});
-			} else {
-				this.setState({ datebaseConnection: true });
-			}
-		});
-	}
+      if (success) {
+        // Get all connected accounts of the user
+        axios.get("/api/accounts").then(res => {
+          // Set user's accounts to state
+          let { accounts } = res.data;
+          if (!accounts) accounts = [];
+          props.updateAccounts(accounts);
+          props.setUser(user);
+          if (user.role === "demo") props.changePage("subscribe");
+          else props.changePage("content");
+          this.setState({ datebaseConnection: true });
+        });
+      } else {
+        this.setState({ datebaseConnection: true });
+      }
+    });
+  }
 
-	signOutOfUsersAccount = () => {
-		axios.get("/api/signOutOfUserAccount").then(res => {
-			let { success, loggedIn, user } = res.data;
-			if (success) {
-				this.props.setUser(user);
-				window.location.reload();
-			} else {
-				if (loggedIn === false) window.location.reload();
-			}
-		});
-	};
-	openHeader = () => {
-		this.props.openHeaderSideBar(true);
-	};
-	openClientSideBar = () => {
-		this.props.openClientSideBar(!this.props.clientSideBar);
-	};
-	render() {
-		const { datebaseConnection } = this.state;
-		const { activePage, clientSideBar, headerSideBar, user } = this.props;
-		let accessClientButton;
-		if (user) {
-			accessClientButton = (user.role === "manager" || user.role === "admin") && (
-				<button className="my-client-button" onClick={() => this.openClientSideBar()}>
-					My Clients
-				</button>
-			);
-		}
-		let margin;
-		if (headerSideBar) margin = { marginLeft: "20%" };
+  signOutOfUsersAccount = () => {
+    axios.get("/api/signOutOfUserAccount").then(res => {
+      let { success, loggedIn, user } = res.data;
+      if (success) {
+        this.props.setUser(user);
+        window.location.reload();
+      } else {
+        if (loggedIn === false) window.location.reload();
+      }
+    });
+  };
+  openHeader = () => {
+    this.props.openHeaderSideBar(true);
+  };
+  openClientSideBar = () => {
+    this.props.openClientSideBar(!this.props.clientSideBar);
+  };
+  render() {
+    const { datebaseConnection } = this.state;
+    const { activePage, clientSideBar, headerSideBar, user } = this.props;
+    let accessClientButton;
+    if (user) {
+      accessClientButton = (user.role === "manager" ||
+        user.role === "admin") && (
+        <button
+          className="my-client-button"
+          onClick={() => this.openClientSideBar()}
+        >
+          My Clients
+        </button>
+      );
+    }
+    let margin;
+    if (headerSideBar) margin = { marginLeft: "20%" };
 
-		if (!datebaseConnection) return <LoaderWedge />;
+    if (!datebaseConnection) return <LoaderWedge />;
 
-		return (
-			<div>
-				<div className="main-navigation-container">
-					{user &&
-						((activePage === "content" ||
-							activePage === "strategy" ||
-							activePage === "newCalendar" ||
-							activePage === "writersBrief" ||
-							activePage === "subscribe" ||
-							activePage === "accounts") &&
-							user.signedInAsUser && (
-								<div className="signed-in-as center">
-									<p>Logged in as: {user.signedInAsUser.fullName}</p>
-									<FontAwesomeIcon
-										icon={faTimes}
-										onClick={() => this.signOutOfUsersAccount()}
-										className="sign-out-of-clients-account"
-									/>
-								</div>
-							))}
-					{accessClientButton}
-					{activePage !== "" &&
-						!headerSideBar && (
-							<FontAwesomeIcon icon={faBars} size="2x" className="activate-header-button" onClick={this.openHeader} />
-						)}
-				</div>
-				{activePage !== "" && headerSideBar && <Header />}
-				{clientSideBar && <ClientsSideBar />}
+    return (
+      <div>
+        <div className="main-navigation-container">
+          {user &&
+            ((activePage === "content" ||
+              activePage === "strategy" ||
+              activePage === "newCalendar" ||
+              activePage === "writersBrief" ||
+              activePage === "subscribe" ||
+              activePage === "accounts") &&
+              user.signedInAsUser && (
+                <div className="signed-in-as center">
+                  <p>Logged in as: {user.signedInAsUser.fullName}</p>
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    onClick={() => this.signOutOfUsersAccount()}
+                    className="sign-out-of-clients-account"
+                  />
+                </div>
+              ))}
+          {accessClientButton}
+          {activePage !== "" &&
+            !headerSideBar && (
+              <FontAwesomeIcon
+                icon={faBars}
+                size="2x"
+                className="activate-header-button"
+                onClick={this.openHeader}
+              />
+            )}
+        </div>
+        {activePage !== "" && headerSideBar && <Header />}
+        {clientSideBar && <ClientsSideBar />}
 
-				{activePage === "" && <LoginPage margin={margin} />}
-				{activePage === "subscribe" && <Subscribe margin={margin} />}
-				{activePage === "content" && <Content margin={margin} />}
-				{activePage === "strategy" && <Strategy margin={margin} />}
-				{activePage === "analytics" && <Analytics margin={margin} />}
-				{activePage === "accounts" && <Accounts margin={margin} />}
-				{activePage === "writersBrief" && <WritersBrief margin={margin} />}
-				{activePage === "manage" && <Manage margin={margin} />}
-				{activePage === "profile" && <Profile margin={margin} />}
-			</div>
-		);
-	}
+        {activePage === "" && <LoginPage margin={margin} />}
+        {activePage === "subscribe" && <Subscribe margin={margin} />}
+        {activePage === "content" && <Content margin={margin} />}
+        {activePage === "strategy" && <Strategy margin={margin} />}
+        {activePage === "analytics" && <Analytics margin={margin} />}
+        {activePage === "accounts" && <Accounts margin={margin} />}
+        {activePage === "writersBrief" && <WritersBrief margin={margin} />}
+        {activePage === "manage" && <Manage margin={margin} />}
+        {activePage === "profile" && <Profile margin={margin} />}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		activePage: state.activePage,
-		user: state.user,
-		clientSideBar: state.clientSideBar,
-		headerSideBar: state.headerSideBar
-	};
+  return {
+    activePage: state.activePage,
+    user: state.user,
+    clientSideBar: state.clientSideBar,
+    headerSideBar: state.headerSideBar
+  };
 }
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators(
-		{
-			changePage: changePage,
-			setUser: setUser,
-			updateAccounts: updateAccounts,
-			openHeaderSideBar: openHeaderSideBar,
-			openClientSideBar: openClientSideBar
-		},
-		dispatch
-	);
+  return bindActionCreators(
+    {
+      changePage: changePage,
+      setUser: setUser,
+      updateAccounts: updateAccounts,
+      openHeaderSideBar: openHeaderSideBar,
+      openClientSideBar: openClientSideBar
+    },
+    dispatch
+  );
 }
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Routes);
