468d6671b4ba3388829ca583389727c2c154dbdc
Patrick Holland <patrickeholland@gmail.com>
2018-09-21 13:04:58 -0700

    page metrics available periods in my notes

--- services/accountFunctions.js
+++ services/accountFunctions.js
@@ -80,135 +80,135 @@ module.exports = {
       */
       FB.api(
         account.socialID +
-          "/insights?metric=" +
-          /*"page_content_activity_by_action_type_unique" +
-          ",page_content_activity_by_age_gender_unique" +
-          ",page_content_activity_by_city_unique" +
-          ",page_content_activity_by_country_unique" +
-          ",page_content_activity_by_locale_unique" +
-          ",page_content_activity" +*/
-          ",page_impressions" +
-          ",page_impressions_unique" +
-          /*",page_impressions_paid" +
-          ",page_impressions_paid_unique" +
-          ",page_impressions_organic" +
-          ",page_impressions_organic_unique" +
-          ",page_impressions_viral" +
-          ",page_impressions_viral_unique" +
-          ",page_impressions_nonviral" +
-          ",page_impressions_nonviral_unique" +
-          ",page_impressions_by_city_unique" +
-          ",page_impressions_by_country_unique" +
-          ",page_impressions_by_locale_unique" +
-          ",page_impressions_by_age_gender_unique" +*/
-          ",page_engaged_users" +
-          ",page_post_engagements" +
-          ",page_consumptions" +
-          /*",page_consumptions_unique" +
-          ",page_consumptions_by_consumption_type" +
-          ",page_consumptions_by_consumption_type_unique" +
-          ",page_places_checkins_by_age_gender" +
-          ",page_places_checkin_total_unique" +
-          ",page_places_checkin_mobile" +
-          ",page_places_checkin_mobile_unique" +
-          ",page_places_checkins_by_age_gender" +
-          ",page_places_checkins_by_locale" +
-          ",page_places_checkins_by_country" +
-          ",page_negative_feedback" +
-          ",page_negative_feedback_unique" +
-          ",page_negative_feedback_by_type" +
-          ",page_negative_feedback_by_type_unique" +
-          ",page_positive_feedback_by_type" +
-          ",page_positive_feedback_by_type_unique" +*/
-          ",page_fans_online" +
-          ",page_fans_online_per_day" +
-          /*",page_fan_adds_by_paid_non_paid_unique" +
-          ",page_actions_post_reactions_like_total" +
-          ",page_actions_post_reactions_love_total" +
-          ",page_actions_post_reactions_wow_total" +
-          ",page_actions_post_reactions_haha_total" +
-          ",page_actions_post_reactions_sorry_total" +
-          ",page_actions_post_reactions_anger_total" +
-          ",page_actions_post_reactions_total" +*/
-          ",page_total_actions" +
-          ",page_cta_clicks_logged_in_total" +
-          ",page_cta_clicks_logged_in_unique" +
-          /*",page_cta_clicks_by_site_logged_in_unique" +
-          ",page_cta_clicks_by_age_gender_logged_in_unique" +
-          ",page_cta_clicks_logged_in_by_country_unique" +
-          ",page_cta_clicks_logged_in_by_city_unique" +
-          ",page_call_phone_clicks_logged_in_unique" +
-          ",page_call_phone_clicks_by_age_gender_logged_in_unique" +
-          ",page_call_phone_clicks_logged_in_by_country_unique" +
-          ",page_call_phone_clicks_logged_in_by_city_unique" +
-          ",page_call_phone_clicks_by_site_logged_in_unique" +
-          ",page_get_directions_clicks_logged_in_unique" +
-          ",page_get_directions_clicks_by_age_gender_logged_in_unique" +
-          ",page_get_directions_clicks_logged_in_by_country_unique" +
-          ",page_get_directions_clicks_logged_in_by_city_unique" +
-          ",page_get_directions_clicks_by_site_logged_in_unique" +
-          ",page_website_clicks_logged_in_unique" +
-          ",page_website_clicks_by_age_gender_logged_in_unique" +
-          ",page_website_clicks_logged_in_by_country_unique" +
-          ",page_website_clicks_logged_in_by_city_unique" +
-          ",page_website_clicks_by_site_logged_in_unique" +*/
-          ",page_fans" +
-          /*",page_fans_locale" +
-          ",page_fans_city" +
-          ",page_fans_country" +
-          ",page_fans_gender_age" +
-          ",page_fan_adds" +
-          ",page_fan_adds_unique" +
-          ",page_fans_by_like_source" +
-          ",page_fans_by_like_source_unique" +
-          ",page_fan_removes" +
-          ",page_fan_removes_unique" +
-          ",page_fans_by_unlike_source_unique" +*/
-          ",page_views_total" +
-          ",page_views_logout" +
-          ",page_views_logged_in_total" +
-          /*",page_views_logged_in_unique" +
-          ",page_views_external_referrals" +
-          ",page_views_by_profile_tab_total" +
-          ",page_views_by_profile_tab_logged_in_unique" +
-          ",page_views_by_internal_referer_logged_in_unique" +
-          ",page_views_by_site_logged_in_unique" +
-          ",page_views_by_age_gender_logged_in_unique" +
-          ",page_video_views" +
-          ",page_video_views_paid" +
-          ",page_video_views_organic" +
-          ",page_video_views_by_paid_non_paid" +
-          ",page_video_views_autoplayed" +
-          ",page_video_views_click_to_play" +
-          ",page_video_views_unique" +
-          ",page_video_repeat_views" +
-          ",page_video_complete_views_30s" +
-          ",page_video_complete_views_30s_paid" +
-          ",page_video_complete_views_30s_organic" +
-          ",page_video_complete_views_30s_autoplayed" +
-          ",page_video_complete_views_30s_click_to_play" +
-          ",page_video_complete_views_30s_unique" +
-          ",page_video_complete_views_30s_repeat_views" +
-          ",page_video_views_10s" +
-          ",page_video_views_10s_paid" +
-          ",page_video_views_10s_organic" +
-          ",page_video_views_10s_autoplayed" +
-          ",page_video_views_10s_click_to_play" +
-          ",page_video_views_10s_unique" +
-          ",page_video_views_10s_repeat" +
-          ",page_video_view_time" +*/
-          ",page_posts_impressions" +
-          ",page_posts_impressions_unique" +
-          /*",page_posts_impressions_paid" +
-          ",page_posts_impressions_paid_unique" +
-          ",page_posts_impressions_organic" +
-          ",page_posts_impressions_organic_unique" +
-          ",page_posts_served_impressions_organic_unique" +
-          ",page_posts_impressions_viral" +
-          ",page_posts_impressions_viral_unique" +
-          ",page_posts_impressions_nonviral" +
-          ",page_posts_impressions_nonviral_unique" +
-          ",page_posts_impressions_frequency_distribution" +*/
+        "/insights?metric=" +
+        "page_content_activity_by_action_type_unique" + // all 3
+        ",page_content_activity_by_age_gender_unique" + // none
+        ",page_content_activity_by_city_unique" + // none
+        ",page_content_activity_by_country_unique" + // none
+        ",page_content_activity_by_locale_unique" + // none
+        ",page_content_activity" + // all 3
+        ",page_impressions" + // all 3
+        ",page_impressions_unique" + // all 3
+        ",page_impressions_paid" + // all 3
+        ",page_impressions_paid_unique" + // all 3
+        ",page_impressions_organic" + // all 3
+        ",page_impressions_organic_unique" + // all 3
+        ",page_impressions_viral" + // all 3
+        ",page_impressions_viral_unique" + // all 3
+        ",page_impressions_nonviral" + // all 3
+        ",page_impressions_nonviral_unique" + // all 3
+        ",page_impressions_by_city_unique" + // none
+        ",page_impressions_by_country_unique" + // none
+        ",page_impressions_by_locale_unique" + // none
+        ",page_impressions_by_age_gender_unique" + // none
+        ",page_engaged_users" + // all 3
+        ",page_post_engagements" + // all 3
+        ",page_consumptions" + // all 3
+        ",page_consumptions_unique" + // all 3
+        ",page_consumptions_by_consumption_type" + // all 3
+        ",page_consumptions_by_consumption_type_unique" + // all 3
+        ",page_places_checkins_by_age_gender" + // none
+        ",page_places_checkin_total_unique" + // all 3
+        ",page_places_checkin_mobile" + // all 3
+        ",page_places_checkin_mobile_unique" + // all 3
+        ",page_places_checkins_by_age_gender" + // none
+        ",page_places_checkins_by_locale" + // none
+        ",page_places_checkins_by_country" + // none
+        ",page_negative_feedback" + // all 3
+        ",page_negative_feedback_unique" + // all 3
+        ",page_negative_feedback_by_type" + // all 3
+        ",page_negative_feedback_by_type_unique" + // all 3
+        ",page_positive_feedback_by_type" + // all 3
+        ",page_positive_feedback_by_type_unique" + // all 3
+        ",page_fans_online" + // day
+        ",page_fans_online_per_day" + // none
+        ",page_fan_adds_by_paid_non_paid_unique" + // day
+        ",page_actions_post_reactions_like_total" + // all 3
+        ",page_actions_post_reactions_love_total" + // all 3
+        ",page_actions_post_reactions_wow_total" + // all 3
+        ",page_actions_post_reactions_haha_total" + // all 3
+        ",page_actions_post_reactions_sorry_total" + // all 3
+        ",page_actions_post_reactions_anger_total" + // all 3
+        ",page_actions_post_reactions_total" + // day
+        ",page_total_actions" + // all 3
+        ",page_cta_clicks_logged_in_total" + // all 3
+        ",page_cta_clicks_logged_in_unique" + // all 3
+        ",page_cta_clicks_by_site_logged_in_unique" + // all 3
+        ",page_cta_clicks_by_age_gender_logged_in_unique" + // none
+        ",page_cta_clicks_logged_in_by_country_unique" + // none
+        ",page_cta_clicks_logged_in_by_city_unique" + // none
+        ",page_call_phone_clicks_logged_in_unique" + // all 3
+        ",page_call_phone_clicks_by_age_gender_logged_in_unique" + // none
+        ",page_call_phone_clicks_logged_in_by_country_unique" + // none
+        ",page_call_phone_clicks_logged_in_by_city_unique" + // none
+        ",page_call_phone_clicks_by_site_logged_in_unique" + // all 3
+        ",page_get_directions_clicks_logged_in_unique" + // all 3
+        ",page_get_directions_clicks_by_age_gender_logged_in_unique" + // none
+        ",page_get_directions_clicks_logged_in_by_country_unique" + // none
+        ",page_get_directions_clicks_logged_in_by_city_unique" + // none
+        ",page_get_directions_clicks_by_site_logged_in_unique" + // all 3
+        ",page_website_clicks_logged_in_unique" + // all 3
+        ",page_website_clicks_by_age_gender_logged_in_unique" + // none
+        ",page_website_clicks_logged_in_by_country_unique" + // none
+        ",page_website_clicks_logged_in_by_city_unique" + // none
+        ",page_website_clicks_by_site_logged_in_unique" + // all 3
+        ",page_fans" + // lifetime
+        ",page_fans_locale" + // none
+        ",page_fans_city" + // none
+        ",page_fans_country" + // none
+        ",page_fans_gender_age" + // none
+        ",page_fan_adds" + // day
+        ",page_fan_adds_unique" + // all 3
+        ",page_fans_by_like_source" + // day
+        ",page_fans_by_like_source_unique" + // day
+        ",page_fan_removes" + // day
+        ",page_fan_removes_unique" + // all 3
+        ",page_fans_by_unlike_source_unique" + // day
+        ",page_views_total" + // all 3
+        ",page_views_logout" + // none
+        ",page_views_logged_in_total" + // all 3
+        ",page_views_logged_in_unique" + // all 3
+        ",page_views_external_referrals" + // day
+        ",page_views_by_profile_tab_total" + // all 3
+        ",page_views_by_profile_tab_logged_in_unique" + // all 3
+        ",page_views_by_internal_referer_logged_in_unique" + // all 3
+        ",page_views_by_site_logged_in_unique" + // all 3
+        ",page_views_by_age_gender_logged_in_unique" + // none
+        ",page_video_views" + // all 3
+        ",page_video_views_paid" + // all 3
+        ",page_video_views_organic" + // all 3
+        ",page_video_views_by_paid_non_paid" + // all 3
+        ",page_video_views_autoplayed" + // all 3
+        ",page_video_views_click_to_play" + // all 3
+        ",page_video_views_unique" + // all 3
+        ",page_video_repeat_views" + // all 3
+        ",page_video_complete_views_30s" + // all 3
+        ",page_video_complete_views_30s_paid" + // all 3
+        ",page_video_complete_views_30s_organic" + // all 3
+        ",page_video_complete_views_30s_autoplayed" + // all 3
+        ",page_video_complete_views_30s_click_to_play" + // all 3
+        ",page_video_complete_views_30s_unique" + // all 3
+        ",page_video_complete_views_30s_repeat_views" + // all 3
+        ",page_video_views_10s" + // all 3
+        ",page_video_views_10s_paid" + // all 3
+        ",page_video_views_10s_organic" + // all 3
+        ",page_video_views_10s_autoplayed" + // all 3
+        ",page_video_views_10s_click_to_play" + // all 3
+        ",page_video_views_10s_unique" + // all 3
+        ",page_video_views_10s_repeat" + // all 3
+        ",page_video_view_time" + // day
+        ",page_posts_impressions" + // all 3
+        ",page_posts_impressions_unique" + // all 3
+        ",page_posts_impressions_paid" + // all 3
+        ",page_posts_impressions_paid_unique" + // all 3
+        ",page_posts_impressions_organic" + // all 3
+        ",page_posts_impressions_organic_unique" + // all 3
+        ",page_posts_served_impressions_organic_unique" + // all 3
+        ",page_posts_impressions_viral" + // all 3
+        ",page_posts_impressions_viral_unique" + // all 3
+        ",page_posts_impressions_nonviral" + // all 3
+        ",page_posts_impressions_nonviral_unique" + // all 3
+        ",page_posts_impressions_frequency_distribution" + // all 3
           "&date_preset=last_3d",
         "get",
         function(res) {
@@ -222,9 +222,7 @@ module.exports = {
 
           for (let index = 0; index < testArray.length; index++) {
             let testObject = testArray[index];
-            if (testObject.title === "Lifetime Total Likes") {
-              console.log(testObject);
-            }
+            console.log(testObject);
             if (testObject.period === "day") {
               dayCount++;
             } else if (testObject.period === "week") {
