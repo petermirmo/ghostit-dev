8356f74a265362eb7dee2daa39a1c550b54980a3
Peter Mirmotahari <peter.mirmotahari@gmail.com>
2020-10-28 11:11:12 -0700

    Adding cooper pacific's designs

--- client/src/website/Demo/index.js
+++ client/src/website/Demo/index.js
@@ -5,29 +5,58 @@ import { Link } from "react-router-dom";
 import Page from "../../components/containers/Page";
 import GIContainer from "../../components/containers/GIContainer";
 
-class ViewWebsiteBlog extends Component {
+class Demo extends Component {
   render() {
     const { location } = this.props;
     const { pathname } = location;
-    let number = 1;
-    if (pathname) number = Number(pathname.slice(21, pathname.length));
+    let lastUrlPart;
+    if (pathname) lastUrlPart = pathname.slice(9, pathname.length);
+    console.log(lastUrlPart);
 
     let src = "";
-    if (number === 1)
+    if (lastUrlPart === "about-us")
       src =
-        "https://res.cloudinary.com/ghostit-co/image/upload/v1603231615/Revised-homepage.jpg";
-    if (number === 2)
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907816/About-Us-Page-Option_01.jpg";
+    if (lastUrlPart === "about-us-2")
       src =
-        "https://res.cloudinary.com/ghostit-co/image/upload/v1603231602/Homepage-Option_02.jpg";
-    if (number === 3)
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907826/About-Us-Page-Option_02.jpg";
+    if (lastUrlPart === "blog")
       src =
-        "https://res.cloudinary.com/ghostit-co/image/upload/v1603231600/Homepage-Option_01.jpg";
-    if (number === 4)
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907848/Blog-Page-Option_01.jpg";
+    if (lastUrlPart === "blog-2")
       src =
-        "https://res.cloudinary.com/ghostit-co/image/upload/v1603231606/Homepage-Option_04.jpg";
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907849/Blog-Page-Option_02.jpg";
+    if (lastUrlPart === "borrowing")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907840/Borrowing-Page-Option_01.jpg";
+    if (lastUrlPart === "borrowing-2")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907846/Borrowing-Page-Option_02.jpg";
+    if (lastUrlPart === "contact-us")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907848/Contact-Us-Page-Option_01.jpg";
+    if (lastUrlPart === "contact-us-2")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907849/Contact-Us-Page-Option_02.jpg";
+    if (lastUrlPart === "investing")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907852/Investing-Page-Option_01.jpg";
+    if (lastUrlPart === "investing-2")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907841/Investing-Page-Option_02.jpg";
+    if (lastUrlPart === "projects")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907827/Projects-Page-Option_01.jpg";
+    if (lastUrlPart === "projects-2")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907828/Projects-Page-Option_02.jpg";
+    if (lastUrlPart === "home")
+      src =
+        "https://res.cloudinary.com/ghostit-co/image/upload/v1603907837/Revised-homepage-V1.jpg";
 
+    if (!lastUrlPart) return <div>hello world</div>;
     return <img className="x-fill" src={src} />;
   }
 }
 
-export default ViewWebsiteBlog;
+export default Demo;
